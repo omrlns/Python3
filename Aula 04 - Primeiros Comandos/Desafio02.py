50290ff0663fe578a1ea95713f07a2b9f1cd7c23
Marlon <marlon.da.silva996@gmail.com>
2023-06-11 22:18:18 -0300

    aula 4

---
+++ Aula 04 - Primeiros Comandos/Desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia do seu nascimento?')
+mes = input('Em que mês você nasceu?')
+ano = input('Em que ano voê nasceu?')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '.',  'Correto?')
---
+++ Aula 04 - Primeiros Comandos/Desafio03.py
@@ -0,0 +1,5 @@
+#foi lançado um desafio sem ter apresentado o recurso para resolução, foi de propósito, para criar um gancho para a próxima aula.
+
+#numero1 = input('Digite o primeiro valor...')
+#numero2 = input('Digite o segundo valor...')
+#print(numero1 + numero2) 
