0d88afb953f1009fd62bff50eb1c42a5e475c5ce
Marlon <marlon.da.silva996@gmail.com>
2024-02-13 00:06:57 -0300

    teste de desenvolvimento do EX023

--- Curso em Vídeo/Exercícios/EX023 - Separando Dígitos de um Número.py
+++ Curso em Vídeo/Exercícios/EX023 - Separando Dígitos de um Número.py
@@ -1,2 +1,12 @@
 # Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 
+numero = int(input('INDIQUE UM NÚMERO DE 0 A 9999: '))
+print('O NÚMERO É {}' .format(numero))
+
+
+
+print('O NÚMERO {} TEM AS SEGUINTES CARACTERÍSTICAS:' .format(numero))
+print('DEZENA: {}' .format(milhar))
+print('DEZENA: {}' .format(centena))
+print('DEZENA: {}' .format(dezena))
+print('DEZENA: {}' .format(unidade))
