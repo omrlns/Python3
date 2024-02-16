6d4f66271392ffae200ecaa85236b537169e5d54
Marlon <marlon.da.silva996@gmail.com>
2024-02-16 20:50:24 -0300

    ajustes do EX025

--- Curso em Vídeo/Exercícios/EX025 - Procurando uma String Dentro de Outra.py
+++ Curso em Vídeo/Exercícios/EX025 - Procurando uma String Dentro de Outra.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 
-cidade = str(input('DIGITE O NOME DA SUA CIDADE: '))
+cidade = str(input('DIGITE O SEU NOME: '))
 validacao ='SILVA' in cidade.upper()
 print('BUSCAMOS POR "SILVA" NO NOME E O RESULTADO É {}' .format(validacao))
 
