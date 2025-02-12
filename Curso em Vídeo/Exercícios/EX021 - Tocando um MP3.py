771e13da0d120066212410010fa2c2700a7db1bd
Marlon <marlon.da.silva996@gmail.com>
2025-02-12 01:34:15 -0300

    tranformando o EX021 em comentário

--- Curso em Vídeo/Exercícios/EX021 - Tocando um MP3.py
+++ Curso em Vídeo/Exercícios/EX021 - Tocando um MP3.py
@@ -1,10 +1,10 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
 #não funciona :/
-import pygame
-pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+# import pygame
+# pygame.init()
+# pygame.mixer.music.load('ex021.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
 
 
