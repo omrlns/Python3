62591871254141c6bd3e83fa09a0c51e9f480c64
Marlon <marlon.da.silva996@gmail.com>
2025-02-12 02:22:11 -0300

    revisão do Desafio05

--- Curso em Vídeo/Aula 06/Desafio05.py
+++ Curso em Vídeo/Aula 06/Desafio05.py
@@ -1,19 +1,19 @@
-#n1 = input('Digite um valor:')
-#n2 = input('Digite outro valor:')
-#soma = n1 + n2
-#print('O resultado da operação é {}'.format(soma))
+# n1 = input('Digite um valor: ')
+# n2 = input('Digite outro valor: ')
+# soma = n1 + n2
+# print('O resultado da operação é {}'.format(soma))
 
-#n1 = int(input('Digite um valor:'))
-#n2 = int(input('Digite outro valor:'))
-#soma = n1 + n2
-#print('O resultado da operação é {}'.format(soma))
-#print('A soma entre {} e {} é {}'.format(n1, n2, soma))
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+soma = n1 + n2
+print('O resultado da operação é {}'.format(soma))
+print('A soma entre {} e {} é {}'.format(n1, n2, soma))
 
-#n = input('Digite algo: ')
-#print(n.isnumeric()) - é número?
-#print(n.isalpha()) - é alfabeto?
-#print(n.isalnum()) - é alfanumérico?
-#print(n.isupper()) - tem letra maiúscula?
+n = input('Digite algo: ')
+print(n.isnumeric()) #é número?
+print(n.isalpha()) #é alfabeto?
+print(n.isalnum()) #é alfanumérico?
+print(n.isupper()) #tem letra maiúscula?
 #dentre outros "is"
 
 #something = input('Digite algo:')
@@ -23,6 +23,3 @@
 #print(something.isalpha()) #tem letra?
 #3
 #print(something.isnumeric()) #tem número/é número?
-
-
-
