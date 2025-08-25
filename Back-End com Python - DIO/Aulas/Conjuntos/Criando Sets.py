bc240cbb6bd4584ed3aa89e6455b102b5c53c04a
omrlns <marlon.da.silva996@gmail.com>
2025-08-24 21:45:20 -0300

    feat: add folder "aulas"/"conjuntos" aula sobre sets

---
+++ Back-End com Python - DIO/Aulas/Conjuntos/Criando Sets.py
@@ -0,0 +1,6 @@
+set([1, 2, 1, 3, 4]) # {1, 2, 3, 4}
+
+set('abacaxi') # {'a', 'c', 'b', 'x', 'i'}
+# o set não necessariamente garante a ordem correta dos valores.
+
+set(('palio', 'gol', 'celta', 'palio')) # {'celta', 'gol', 'palio'}
