ea78139edd4833a27c0eafd36c261e94c2d9d3a9
omrlns <marlon.da.silva996@gmail.com>
2025-08-24 22:06:06 -0300

    feat: add remove()

---
+++ Back-End com Python - DIO/Aulas/Conjuntos/remove().py
@@ -0,0 +1,5 @@
+A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# elimina o valor que seja compatível com o parâmetro informado
+A.remove(10) # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+A.remove(1) # {2, 3, 4, 5, 6, 7, 8, 9}
