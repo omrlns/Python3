78b0cc86a94bd8316a82fee978faade9b73c4c43
omrlns <marlon.da.silva996@gmail.com>
2025-08-24 22:01:11 -0300

    feat: add comment in issuperset()

--- Back-End com Python - DIO/Aulas/Conjuntos/issuperset.py
+++ Back-End com Python - DIO/Aulas/Conjuntos/issuperset.py
@@ -1,3 +1,8 @@
+# o método issuperset() verifica se um conjunto é um superconjunto de outro. 
+# um conjunto A é um superconjunto de um conjunto B 
+# se todos os elementos de B também estão em A. 
+# isso retorna True se A for um superconjunto de B, e False caso contrário.
+
 A = {1, 2, 3}
 B = {1, 2}
 C = {1, 4}
