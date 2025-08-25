baf0ee454ee21fc99e3786d9a4fae209cc79770a
omrlns <marlon.da.silva996@gmail.com>
2025-08-24 21:57:23 -0300

    feat: add comment in issubset()

--- Back-End com Python - DIO/Aulas/Conjuntos/issubset().py
+++ Back-End com Python - DIO/Aulas/Conjuntos/issubset().py
@@ -1,3 +1,8 @@
+# o método issubset() verifica se um conjunto é um subconjunto de outro. 
+# um conjunto A é um subconjunto de um conjunto B 
+# se todos os elementos de A também estão em B. 
+# isso retorna True se A for um subconjunto de B, e False caso contrário.
+
 A = {1, 2, 8}
 B = {1, 2, 3, 8, 9, 0}
 
