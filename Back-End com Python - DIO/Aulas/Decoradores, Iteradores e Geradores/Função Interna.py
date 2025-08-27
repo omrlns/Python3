a8ba8f415604f44a2a4763f31d64a64fade47aa2
omrlns <marlon.da.silva996@gmail.com>
2025-08-27 09:19:03 -0300

    feat: add código da aula de funções internas

---
+++ Back-End com Python - DIO/Aulas/Decoradores, Iteradores e Geradores/Função Interna.py
@@ -0,0 +1,15 @@
+def pai():
+    print('EU SOU O PAI DE TODOS!')
+
+    def filho():
+        print('EU SOU FILHO DO CARA QUE É PAI!')
+        
+
+    def neto():
+        print('EU SOU FILHO DO PAI QUE É FILHO DO PAI DE TODOS!')
+
+    filho()
+    neto()
+
+
+pai()
