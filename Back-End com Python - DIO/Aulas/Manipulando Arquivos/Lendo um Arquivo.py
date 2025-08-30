d81635ebaa54cfb76915577d884399ae9bdd7cb2
omrlns <marlon.da.silva996@gmail.com>
2025-08-29 23:59:17 -0300

    feat: add relative path

--- Back-End com Python - DIO/Aulas/Manipulando Arquivos/Lendo um Arquivo.py
+++ Back-End com Python - DIO/Aulas/Manipulando Arquivos/Lendo um Arquivo.py
@@ -1,6 +1,6 @@
 #  ler todo o conteúdo do arquivo de uma vez
-file = open('exemplo.txt', 'r') # não vai rodar por conta da posição do arquivo
+file = open('Aulas\Manipulando Arquivos/exemplo.txt', 'r')
 print(file.read())
-print(file.readline()) # lê o arquivo linha por linha
-print(file.readlines()) # lê tudo, porque retorna o item como uma lista completa
+# print(file.readline()) # lê o arquivo linha por linha
+# print(file.readlines()) # lê tudo, porque retorna o item como uma lista completa
 file.close
