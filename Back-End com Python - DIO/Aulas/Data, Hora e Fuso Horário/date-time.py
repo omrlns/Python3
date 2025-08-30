b26d69506d75343ac4aabe853a0aac483fbdafd6
omrlns <marlon.da.silva996@gmail.com>
2025-08-29 22:58:20 -0300

    feat: add código da aula sobre datetime

---
+++ Back-End com Python - DIO/Aulas/Data, Hora e Fuso Horário/date-time.py
@@ -0,0 +1,10 @@
+from datetime import date, datetime
+
+dia = date(2025, 8, 14)
+print(dia)
+print(date.today())
+
+dataHora = datetime(2025, 8, 14, 11, 25, 00)
+print(dataHora)
+print(datetime.today())
+
